be0b626196a8f07e6c8fba89c7bc423b72ccc0c4
Syed Ismail <syedismailpasha451@gmail.com>
2026-01-28 16:59:36 +0530

    Add AWS provider and IAM user configuration

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-resource "local_file" "my_pets" {
-  filename = var.filename
-  content  = var.content
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "admin_user" {
+  name = "Syed"
+
+  tags = {
+    description = "Technical Team Lead"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "syed_s3_full_access" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
